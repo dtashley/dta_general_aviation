a5d0ccd0cc12dc02c90007dc2c039bf7821bb7b6
David T. Ashley <dashley@gmail.com>
2020-07-30 21:09:02 -0400

    Edits.

--- 2020_ppl_renew/rvw_flt_notes/recomp.bat
+++ 2020_ppl_renew/rvw_flt_notes/recomp.bat
@@ -1,3 +1,16 @@
-latex 2020_ppl_renew.tex
-latex 2020_ppl_renew.tex
+rem ---------------------------------------------------------------------------
+rem Run three times to be sure index entries are correct.
+rem ---------------------------------------------------------------------------
+latex 2020_ppl_renew
+makeindex 2020_ppl_renew
+latex 2020_ppl_renew
+makeindex 2020_ppl_renew
+latex 2020_ppl_renew
+makeindex 2020_ppl_renew
+rem ---------------------------------------------------------------------------
+rem Create Postscript output.
+rem ---------------------------------------------------------------------------
 dvips -T 8.5in,11in 2020_ppl_renew
+rem ---------------------------------------------------------------------------
+rem Creation of a .PDF will be done manually.
+rem ---------------------------------------------------------------------------
